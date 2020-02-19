7ce5d2dbb8d4d4146ff72b355f1d27468c745c35
oleg <soe190715@gmail.com>
2020-02-19 18:38:06 +0700

    models ide_helper

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,12 +4,35 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Models\Traits\DataTimeTrait;
+use Eloquent;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Class Project
+ *
  * @package App\Models
+ * @property int $id
+ * @property string $name Имя проекта
+ * @property string $user_id Владелец
+ * @property string|null $note Описание. Замечаение.
+ * @property string|null $url Сайт проекта
+ * @property array|null $data Доп.данные. Резерв
+ * @property string $created_at
+ * @property string $updated_at
+ * @property-read User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereData($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereNote($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereUserId($value)
+ * @mixin Eloquent
  */
 class Project extends Model
 {
