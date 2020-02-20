0b7cb0be804b8159567a984bbd3add13cec8d2ff
oleg <soe190715@gmail.com>
2020-02-20 14:52:00 +0700

    make packages

---
+++ packages/soe-smile/content-watch/src/ContentWatchServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace SoeSmile\ContentWatch;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ContentWatchServiceProvider
+ * @package SoeSmile\ContentWatch
+ */
+class ContentWatchServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+
+    public function register()
+    {
+    }
+}
